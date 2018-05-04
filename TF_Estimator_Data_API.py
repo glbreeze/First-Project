2c6485d9b747601b3067154f48735b71d1cbacfc
Li Guo <gl.breeze@aliyun.com>
2018-05-04 10:23:24 +0800

    Update TF_Estimator_Data_API.py

--- TF_Estimator_Data_API.py
+++ TF_Estimator_Data_API.py
@@ -1,168 +1,166 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+"""
+original TFIDF model
+"""
 
-import argparse
-import shutil
-import sys
+import os; os.environ['OMP_NUM_THREADS'] = '1'
+from contextlib import contextmanager
+from functools import partial
+from operator import itemgetter
+from multiprocessing.pool import ThreadPool
+import time
+from typing import List, Dict
+
+import keras as ks
+import pandas as pd
 import numpy as np
-
 import tensorflow as tf
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer as Tfidf
+from sklearn.pipeline import make_pipeline, make_union, Pipeline, FeatureUnion
+from sklearn.preprocessing import FunctionTransformer, StandardScaler
+from sklearn.metrics import mean_squared_log_error
+from sklearn.model_selection import KFold
+from sklearn.base import TransformerMixin
 
-_CSV_COLUMNS = [
-    'age', 'workclass', 'fnlwgt', 'education', 'education_num',
-    'marital_status', 'occupation', 'relationship', 'race', 'gender',
-    'capital_gain', 'capital_loss', 'hours_per_week', 'native_country',
-    'income_bracket'
-]
+BASE_DIR = os.getcwd()
+BASE_DIR = os.path.dirname(BASE_DIR)
 
-_CSV_COLUMN_DEFAULTS = [[0], [''], [0], [''], [0], [''], [''], [''], [''], [''],
-                        [0], [0], [0], [''], ['']]
+# @contextmanager
+def timer(name):
+    t0 = time.time()
+    yield
+    #print(f'[{name}] done in {time.time() - t0:.0f} s')
 
-########################################################################################################################
+def preprocess(df: pd.DataFrame) -> pd.DataFrame:
+    df['name'] = df['name'].fillna('') + ' ' + df['brand_name'].fillna('')
+    df['text'] = (df['item_description'].fillna('') + ' ' + df['name'] + ' ' + df['category_name'].fillna(''))
+    return df
 
-model_dir = 'D:/Work/Learning/Deep and wide model/census_model'
+def on_field(f: str, *vec) -> Pipeline:
+    return make_pipeline(FunctionTransformer(itemgetter(f), validate=False), *vec)
 
-model_type = 'wide_deep'
+def to_records(df: pd.DataFrame) -> List[Dict]:
+    return df.to_dict(orient='records')
 
-train_epochs = 10
+def fit_predict(xs, y_train) -> np.ndarray:
+    X_train, X_test = xs
+   # config = tf.ConfigProto(
+   #     intra_op_parallelism_threads=1, use_per_session_threads=1, inter_op_parallelism_threads=1)
+    with tf.Session( graph=tf.Graph() ) as sess :
+        ks.backend.set_session(sess)
+        model_in = ks.Input(shape=(X_train.shape[1],), dtype='float32', sparse=True)
+        out = ks.layers.Dense(192, activation='relu')(model_in)
+        out = ks.layers.Dense(64, activation='relu')(out)
+        out = ks.layers.Dense(64, activation='relu')(out)
+        out = ks.layers.Dense(1)(out)
+        model = ks.Model(model_in, out)
+        model.compile(loss='mean_squared_error', optimizer=ks.optimizers.Adam(lr=3e-3))
+        for i in range(3):
+            # with timer(f'epoch {i + 1}'):
+                model.fit(x=X_train, y=y_train, batch_size=2**(11 + i), epochs=1, verbose=0)
+        return model.predict(X_test)[:, 0]
 
-epochs_per_eval = 2
+class ColumnExtractor(TransformerMixin):
+    def __init__(self, cols):
+        self.cols = cols
 
-batch_size = 40
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
 
-train_data = 'D:/Work/Learning/Deep and wide model/data/census_data/adult.data'
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xcols = X[self.cols]
+        return Xcols
 
-test_data = 'D:/Work/Learning/Deep and wide model/data/census_data/adult.test'
+class ToDict(TransformerMixin):
+        def __init__(self, cols):
+            self.cols = cols
 
-_NUM_EXAMPLES = {
-    'train': 32561,
-    'validation': 16281,
-}
+        def fit(self, X, y=None):
+            # stateless transformer
+            return self
 
-########################################################################################################################
-age = tf.feature_column.numeric_column('age')
-education_num = tf.feature_column.numeric_column('education_num')
-capital_gain = tf.feature_column.numeric_column('capital_gain')
-capital_loss = tf.feature_column.numeric_column('capital_loss')
-hours_per_week = tf.feature_column.numeric_column('hours_per_week')
-
-education = tf.feature_column.categorical_column_with_vocabulary_list(
-      'education', [
-          'Bachelors', 'HS-grad', '11th', 'Masters', '9th', 'Some-college',
-          'Assoc-acdm', 'Assoc-voc', '7th-8th', 'Doctorate', 'Prof-school',
-          '5th-6th', '10th', '1st-4th', 'Preschool', '12th'])
-
-marital_status = tf.feature_column.categorical_column_with_vocabulary_list(
-      'marital_status', [
-          'Married-civ-spouse', 'Divorced', 'Married-spouse-absent',
-          'Never-married', 'Separated', 'Married-AF-spouse', 'Widowed'])
-
-relationship = tf.feature_column.categorical_column_with_vocabulary_list(
-      'relationship', [
-          'Husband', 'Not-in-family', 'Wife', 'Own-child', 'Unmarried',
-          'Other-relative'])
-
-workclass = tf.feature_column.categorical_column_with_vocabulary_list(
-      'workclass', [
-          'Self-emp-not-inc', 'Private', 'State-gov', 'Federal-gov',
-          'Local-gov', '?', 'Self-emp-inc', 'Without-pay', 'Never-worked'])
-
-# To show an example of hashing:
-occupation = tf.feature_column.categorical_column_with_hash_bucket(
-      'occupation', hash_bucket_size=1000)
-
-# Transformations.
-age_buckets = tf.feature_column.bucketized_column(
-      age, boundaries=[18, 25, 30, 35, 40, 45, 50, 55, 60, 65])
-
-# Wide columns and deep columns.
-base_columns = [
-      education, marital_status, relationship, workclass, occupation, age_buckets,
-  ]
-
-crossed_columns = [
-      tf.feature_column.crossed_column(
-          ['education', 'occupation'], hash_bucket_size=1000),
-      tf.feature_column.crossed_column(
-          [age_buckets, 'education', 'occupation'], hash_bucket_size=1000),
-  ]
-
-wide_columns = base_columns + crossed_columns
-
-deep_columns = [
-      age,
-      education_num,
-      capital_gain,
-      capital_loss,
-      hours_per_week,
-      tf.feature_column.indicator_column(workclass),
-      tf.feature_column.indicator_column(education),
-      tf.feature_column.indicator_column(marital_status),
-      tf.feature_column.indicator_column(relationship),
-      # To show an example of embedding
-      tf.feature_column.embedding_column(occupation, dimension=8),
-  ]
-
-########################################################################################################################
-
-  # Create a tf.estimator.RunConfig to ensure the model is run on CPU, which
-  # trains faster than GPU for this model.
-run_config = tf.estimator.RunConfig().replace(session_config=tf.ConfigProto(device_count={'GPU': 0}))
-
-model_mixed = tf.estimator.DNNLinearCombinedClassifier(
-        model_dir=model_dir,
-        linear_feature_columns=wide_columns,
-        dnn_feature_columns=deep_columns,
-        dnn_hidden_units=[100, 75, 50, 25],
-        config=run_config)
-
-model_linear = tf.estimator.LinearClassifier(
-        model_dir=model_dir,
-        feature_columns=wide_columns,
-        config=run_config)
-
-model_DNN = tf.estimator.DNNClassifier(
-        model_dir=model_dir,
-        feature_columns=deep_columns,
-        hidden_units=[100, 75, 50, 25],
-        config=run_config)
-
-########################################################################################################################
-
-def input_fn(data_file, num_epochs, shuffle, batch_size=40):
-
-  def parse_csv(value):
-    print('Parsing', data_file)
-    columns = tf.decode_csv(value, record_defaults=_CSV_COLUMN_DEFAULTS)
-    features = dict(zip(_CSV_COLUMNS, columns))
-    labels = features.pop('income_bracket')     # response variable
-    return features, tf.equal(labels, '>50K')
-
-  # Extract lines from input files using the Dataset API.
-  dataset = tf.data.TextLineDataset(data_file)
-
-  if shuffle:
-    dataset = dataset.shuffle(buffer_size=_NUM_EXAMPLES['train'])
-
-  dataset = dataset.map(parse_csv, num_parallel_calls=5)  # number of elements to processed in parallel
-
-  # We call repeat after shuffling, rather than before, to prevent separate
-  # epochs from blending together.
-  dataset = dataset.repeat(num_epochs)          # repeat的功能就是将整个序列重复多次
-  dataset = dataset.batch(batch_size)           # batch就是将多个元素组合成batch，每个batch中有batch_size行
-
-  iterator = dataset.make_one_shot_iterator()
-  features, labels = iterator.get_next()
-  return features, labels
+        def transform(self, X):
+            # assumes X is a DataFrame
+            Xdict = X.to_dict('records')
+            return Xdict
 
 
-########################################################################################################################
-#def main(unused_argv):
-  # Clean up the model directory if present
-shutil.rmtree(model_dir, ignore_errors=True)
+vectorizer = FeatureUnion([
+        ('continuous', Pipeline([
+            ('extract', ColumnExtractor('name')),
+            ('tfidf', Tfidf(max_features=100000, token_pattern='\w+'))
+        ])),
+        ('text', Pipeline([
+            ('extract', ColumnExtractor('text')),
+            ('tfidf', Tfidf(max_features=100000, token_pattern='\w+', ngram_range=(1, 2)))
+        ])),
+        ('shippingCondition', Pipeline([
+            ('extract', ColumnExtractor(['shipping', 'item_condition_id'])),
+            ('ToDict', ToDict(['shipping', 'item_condition_id'])),
+            ('DictVectorizer', DictVectorizer())
+        ])),
+    ])
 
-# Train and evaluate the model every `FLAGS.epochs_per_eval` epochs.
-model_linear.train(input_fn=lambda: input_fn(train_data, epochs_per_eval, True, batch_size))    # epoch_per_eval 数据被重复了几次
+y_scaler = StandardScaler()
 
-model.train(input_fn=lambda: input_fn(train_data, epochs_per_eval, True, batch_size))
+
+def load_data():
+    train = pd.read_csv(os.path.join(os.path.join(BASE_DIR, 'Challenge\Data'), 'train_mock.tsv'),
+                            sep='\t', encoding='utf-8')
+    train = train[train['price'] > 0].reset_index(drop=True)
+    train = preprocess(train)
+
+    valid = pd.read_csv(os.path.join(os.path.join(BASE_DIR, 'data'), 'test_mock.tsv'),
+                        sep='\t', encoding='utf-8')
+    valid = valid[valid['price'] > 0].reset_index(drop=True)
+    valid = preprocess(valid)
+
+    return train, valid
+
+def main():
+    train, valid =load_data()
+    y_train = y_scaler.fit_transform(np.log1p(train['price'].values.reshape(-1, 1)))
+    X_train = vectorizer.fit_transform(preprocess(train)).astype(np.float32)
+    print(f'X_train: {X_train.shape} of {X_train.dtype}')
+    #del train
+    X_valid = vectorizer.transform(preprocess(valid)).astype(np.float32)
+    print('X_valid shape:')
+    print(X_valid.shape)
+
+    #################
+    Xb_train, Xb_valid = [x.astype(np.bool).astype(np.float32) for x in [X_train, X_valid]]
+    xsb = [Xb_train, Xb_valid]
+    xs = [X_train, X_valid]
+    y_pred1 = fit_predict(xs, y_train)
+    y_pred2 = fit_predict(xs, y_train)
+    y_pred1b = fit_predict(xsb, y_train)
+    y_pred2b = fit_predict(xsb, y_train)
+
+    y_pred = np.mean(np.array([y_pred1, y_pred2, y_pred1b, y_pred2b]), axis=0)
+    y_pred = np.expm1(y_scaler.inverse_transform(y_pred.reshape(-1, 1))[:, 0])
+    print('Avg Valid RMSLE: {:.4f}'.format(np.sqrt(mean_squared_log_error(valid['price'], y_pred))))
+
+    valid = valid.reset_index(drop=True)
+    result = pd.concat([valid, pd.DataFrame(y_pred, columns=['pred'])], axis=1)
+    result.to_csv(os.path.join(BASE_DIR, 'out/result_avg.csv'))
+
+    y_pred1 = np.expm1(y_scaler.inverse_transform(y_pred1.reshape(-1, 1))[:, 0])
+    print('Avg Valid RMSLE1: {:.4f}'.format(np.sqrt(mean_squared_log_error(valid['price'], y_pred1))))
+    result1 = pd.concat([valid, pd.DataFrame(y_pred1, columns=['pred'])], axis=1)
+    result.to_csv(os.path.join(BASE_DIR, 'outresult_1.csv'))
+
+    y_pred1b = np.expm1(y_scaler.inverse_transform(y_pred1b.reshape(-1, 1))[:, 0])
+    print('Avg Valid RMSLE1: {:.4f}'.format(np.sqrt(mean_squared_log_error(valid['price'], y_pred1b))))
+    result1 = pd.concat([valid, pd.DataFrame(y_pred1b, columns=['pred'])], axis=1)
+    result.to_csv(os.path.join(BASE_DIR, 'out/result_1b.csv'))
+
+    xs_train=[X_train, X_train]
+    ytrain_pred1 = fit_predict(xs_train, y_train)
+    ytrain_pred1 = np.expm1(y_scaler.inverse_transform(ytrain_pred1.reshape(-1, 1))[:, 0])
+    print('Avg Valid RMSLE1: {:.4f}'.format(np.sqrt(mean_squared_log_error(train['price'], ytrain_pred1))))
+    result_train1 = pd.concat([train, pd.DataFrame(ytrain_pred1, columns=['pred'])], axis=1)
+    result.to_csv(os.path.join(BASE_DIR, 'out/result_trian1.csv'))
+
+#############################
